9225f3602bd91abe8341276937cd861ae5d1173b
Phablo Franca <phablo.f00@gmail.com>
2025-04-04 17:58:19 -0300

    Começando o projeto

---
+++ pages/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/home.css">
+    <title>Home</title>
+</head>
+<body>
+    <div class="container">
+        <div class="layout">
+            <div>
+                <input type="text">
+                <button>Pesquisar</button>
+            </div>
+            <div class="filtros-conteudos">
+                <div class="filtros">
+                    <select name="" id="">
+                        <option value=""></option>
+                    </select>
+                    <div>
+                        <input type="text" id="precoMin">
+                        <input type="text" id="precoMax">
+                    </div>
+                    <button>Filtrar</button>
+                </div>
+                <div class="conteudo">
+                    <div class="card">
+                        <img src="https://lh3.googleusercontent.com/proxy/Ojf_nYSR-GwslmcZVexqkijlz2SVM-Xx0CirukSByp9WVCnOwv-rLz2s8KDRiIINjgJlE2uvrEMeTS7yRE-V5Dzscl-wYhu78V1vZ-e_I1lpU9nORInx3l6wFns" alt="">
+                        <h3>Titulo</h3>
+                        <div class="informacoes">
+                            <p>Author</p>
+                            <p>2006</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
