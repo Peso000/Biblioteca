2e778402ed1c059fa847552e5f226d1047442b0f
PHABLO FRANCA <phablo.franca@cs.up.edu.br>
2025-04-07 21:30:06 -0300

    fazendo o header

--- pages/index.php
+++ pages/index.php
@@ -8,34 +8,58 @@
 </head>
 <body>
     <div class="container">
-        <div class="layout">
-            <div class="pesquisa">
-                <input type="text">
-                <button>Pesquisar</button>
+        <header>
+            <div class="titulos">
+                <input class="barra-pesquisa" type="text">
+                <button class="btnPesquisa"><img src="../images/icons8-pesquisar-50.png" alt=""></button>
             </div>
-            <div class="filtros-conteudos">
+            <div>
                 <div class="filtros">
-                    <select name="" id="">
-                        <option value=""></option>
-                    </select>
-                    <div>
-                        <input type="text" id="precoMin">
-                        <input type="text" id="precoMax">
-                    </div>
-                    <button>Filtrar</button>
+                    <form action="./teste.php" method="get">
+
+                        <button name="teste" value="10">Ficção</button>
+
+                    <a href="">
+                        <button>Ação</button>
+                    </a>
+                    <a href="">
+                        <button>Aventura</button>
+                    </a>
+                    <a href="">
+                        <button>Terror</button>
+                    </a>
+                    <a href="">
+                        <button>HQ</button>
+                    </a>
+                    <a href="">
+                        <button>Mangá</button>
+                    </a>
+                    <a href="">
+                        <button>Empreendedorismo</button>
+                    </a>
+                    <a href="">
+                        <button>Auto-ajuda</button>
+                    </a>
+                    <a href="">
+                        <button>Psicologia</button>
+                    </a>
+                    </form>
                 </div>
-                <div class="conteudo">
-                    <?php for($i = 0; $i < 15; $i++): ?>
-                    <div class="card">
-                        <img src="https://lh3.googleusercontent.com/proxy/Ojf_nYSR-GwslmcZVexqkijlz2SVM-Xx0CirukSByp9WVCnOwv-rLz2s8KDRiIINjgJlE2uvrEMeTS7yRE-V5Dzscl-wYhu78V1vZ-e_I1lpU9nORInx3l6wFns" alt="">
-                        <h3>Titulo</h3>
-                        <div class="informacoes">
-                            <p>Author</p>
-                            <p>2006</p>
-                        </div>
+            </div>
+        </header>
+        <div class="layout">
+            <div class="conteudo">
+                <?php for($i = 0; $i < 15; $i++): ?>
+                <div class="card">
+                    <img src="https://lh3.googleusercontent.com/proxy/Ojf_nYSR-GwslmcZVexqkijlz2SVM-Xx0CirukSByp9WVCnOwv-rLz2s8KDRiIINjgJlE2uvrEMeTS7yRE-V5Dzscl-wYhu78V1vZ-e_I1lpU9nORInx3l6wFns" alt="">
+                    <h3>Titulo</h3>
+                    <div class="informacoes">
+                        <p>Author</p>
+                        <p>2006</p>
                     </div>
-                    <?php endfor; ?>
+                    <button>Comprar</button>
                 </div>
+                <?php endfor; ?>
             </div>
         </div>
     </div>
