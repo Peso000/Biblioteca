666d62ced802d38780caabe08fe6a43291b7d466
Phablo <Phablo.f00@gmail.com>
2025-04-09 16:36:20 -0300

    Criando outro carousel

---
+++ component/carrosel2.php
@@ -0,0 +1,19 @@
+<div class="carrosel2">
+    <button onclick="anterior()">Esquerda</button>
+    <div class="items-carrosel2">
+        <div class="container-carrosel">
+            <?php for($i = 0; $i < 15; $i++): ?>
+                <div class="card" id="<?php echo $i; ?>">
+                    <img src="https://lh3.googleusercontent.com/proxy/Ojf_nYSR-GwslmcZVexqkijlz2SVM-Xx0CirukSByp9WVCnOwv-rLz2s8KDRiIINjgJlE2uvrEMeTS7yRE-V5Dzscl-wYhu78V1vZ-e_I1lpU9nORInx3l6wFns" alt="">
+                    <h3>Titulo</h3>
+                    <div class="informacoes">
+                        <p>Author</p>
+                        <p><?php echo $i; ?></p>
+                    </div>
+                    <button>Comprar</button>
+                </div>
+            <?php endfor; ?>
+        </div>
+    </div>
+    <button onclick="proximo()">Direita</button>
+</div>
---
+++ js/home2.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+}
+
+function proximo(){
+    let carrosel = document.querySelector(".items-carrosel2");
+
+    carrosel.scrollLeft += 250;
+}
